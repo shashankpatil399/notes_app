0c82094f367027f425685d36e5f5ccc5629f8948
Shashank Patil <shashankpatil399@gmail.com>
2024-09-11 00:19:24 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/myNotesApp', {
+        await mongoose.connect('mongodb+srv://shashankpatil199:IweaB5OKPEJX5Xmz@cluster0.98tdg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
           });
